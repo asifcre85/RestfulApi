ecdb301cf9a6b6a6b81fee110b0e37d0962ba3a7
asifkottakal1 <asifkottakal1@gmail.com>
2020-05-01 11:26:37 +0530

    first commit

---
+++ app/Http/Controllers/FileController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FileController extends Controller
+{
+    public function countryList(){
+        return response()->download(public_path('download.png'),'User Image');
+    }
+    public function countrySave(Request $request){
+        $fileName="download.png";
+        $path=$request->file('photo')->move(public_path("/"),$fileName);
+        $photoURL=url('/'.$fileName);
+        return response()->json(['url'=>$photoURL], 200);
+    }
+}
